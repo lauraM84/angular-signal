4af4f3bf24f13c55d03ed79e8b5ecd5704b47acb
lauraM84 <laura.mazza84@gmail.com>
2025-04-08 13:25:37 +0200

    non fatto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './components/counter/counter.component';
 import { ControlsComponent } from './components/controls/controls.component';
+import { TestComponent } from './components/test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent, ControlsComponent],
+  imports: [RouterOutlet, CounterComponent, ControlsComponent, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
